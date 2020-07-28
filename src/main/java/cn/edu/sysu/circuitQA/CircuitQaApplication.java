76a86ba28fbd8c8856e54cb3776250bda01c62c2
hejiayang <1017330189@qq.com>
2020-07-28 22:11:29 +0800

    首次上传

---
+++ src/main/java/cn/edu/sysu/circuitQA/CircuitQaApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.sysu.circuitQA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitQaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitQaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/edu/sysu/circuitQA/CircuitQaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.sysu.circuitQA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitQaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
